42c5d443280828b2af33c803752c44e524218ebb
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2026-08-19 05:41:38 +0000

    add custom module

---
+++ kiwiil_financing/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Kawiil Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+    'category': 'Kawiil/Custom Modules',
+    'version': '1.0.0',
+    'author': 'ODOP Trainee',
+    'maintainer': 'Your Name',
+    'depends': ['base'],
+    'license': 'OPL-1',
+    'application': True,
+}
